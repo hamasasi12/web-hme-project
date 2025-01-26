5d4f6e2fd4c686a46fb6d8d9b40a9d6e26f2184e
hamasasi12 <hamasakifsanie@gmail.com>
2025-01-27 01:02:21 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,142 +61,8 @@
         {{-- Profile Video Section --}}
         @include('partials.profileVideo')
 
-        <!-- ======= Portfolio Section ======= -->
-        <section id="portfolio" class="portfolio">
-            <div class="container" data-aos="fade-up">
-
-                <div class="section-title">
-                    <h2>Kegiatan</h2>
-                    <p>Beberapa kegiatan yang diselenggarakan oleh Himpunan Mahasiswa Elektro.</p>
-                </div>
-
-                <ul id="portfolio-flters" class="d-flex justify-content-center" data-aos="fade-up"
-                    data-aos-delay="100">
-                    <li data-filter="*" class="filter-active">All</li>
-                    <li data-filter=".filter-app">Tahunan</li>
-                    <li data-filter=".filter-card">Divisi</li>
-                </ul>
-
-
-                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Pelantikan1.jpg" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Pelantikan Himpunan Mahasiswa Elektro Periode 2024/2025</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/Pelantikan1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/takjil.jpg" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Buka bersama & bagi takjil </h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/takjil.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/workshop.png" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Workshop Curriculum Vitae 2024</h4>
-                            <!--<a href="workshop" class="btn btn-primary btn-sm">-->
-                            <!--  <i class="fas fa-arrow-circle-right"></i> Selengkapnya-->
-                            <!--</a>-->
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/Workshop.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/takjil.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/coming.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/coming.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/coming.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/coming.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/coming.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-img"><img src="assets/img/portfolio/Coming.PNG" class="img-fluid"
-                                alt=""></div>
-                        <div class="portfolio-info">
-                            <h4>Coming Soon</h4>
-                            <!-- <p>App</p> -->
-                            <!--<a href="assets/img/portfolio/coming.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>-->
-                            <!--<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>-->
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-        </section><!-- End Portfolio Section -->
+        {{-- Portofolio Section --}}
+        @include('partials.portofolio')
 
     </main><!-- End #main -->
 
@@ -361,4 +227,27 @@
     </script>
 </body>
 
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div>
+            hamas akif sanie dicoding testing yang akan dilaksnakan sementara ini dengan baik 
+        </div>
+    </header>
+    <main>
+        <div class="">
+
+        </div>
+    </main>
+    <footer></footer>
+</body>
 </html>
