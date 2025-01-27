273409c1d340196c0ca254e0bb471f9a6023c4c2
hamasasi12 <hamasakifsanie@gmail.com>
2025-01-27 14:22:29 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -228,26 +228,3 @@
 </body>
 
 </html>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <div>
-            hamas akif sanie dicoding testing yang akan dilaksnakan sementara ini dengan baik 
-        </div>
-    </header>
-    <main>
-        <div class="">
-
-        </div>
-    </main>
-    <footer></footer>
-</body>
-</html>
